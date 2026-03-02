5ccc580c8279d1725c272830be87c57c9fec576b
biplabsarkar700 <biplabsarkar7001@gmail.com>
2026-03-02 13:00:32 +0530

    M

--- js/script.js
+++ js/script.js
@@ -41,3 +41,19 @@ btn.addEventListener("click", () => {
       carousel.to(parseInt(dot.dataset.index));
     });
   });
+
+
+
+  
+  //FAQ Section
+
+  function toggleFaq(btn) {
+    const item = btn.closest('.faq-item');
+    const isOpen = item.classList.contains('open');
+
+    // Close all
+    document.querySelectorAll('.faq-item').forEach(el => el.classList.remove('open'));
+
+    // Open clicked if it was closed
+    if (!isOpen) item.classList.add('open');
+  }
